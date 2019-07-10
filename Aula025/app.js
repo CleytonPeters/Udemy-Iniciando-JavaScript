5a3df9fb7c1027916be6ca40cde9cd104cc69e04
Cleyton Peters <cleyton.peters@byseven.com.br>
2019-07-10 18:01:05 -0300

    aula25 incompleta

---
+++ Aula025/app.js
@@ -0,0 +1,9 @@
+var mudaCor = function () {
+    var nome = document.getElementById();
+    document.getElementById(nome).style.background = "blue"
+}
+
+if (azul.addEventListener('click', mudaCor))
+    else if (amarelo.addEventListener('click', mudaCor))
+    else if (vermelho.addEventListener('click', mudaCor))
+    else if (preto.addEventListener('click', mudaCor))
